7c1add92d7dab07c90f989a36fe2a80e35438df6
Boris <boris@gridscale.io>
2024-04-30 22:50:09 +0200

    adding .stylua.toml and setting some things to match luarocks styleguide

--- init.lua
+++ init.lua
@@ -64,23 +64,23 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Highlight when yanking (copying) text
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+   desc = "Highlight when yanking (copying) text",
+   group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+   callback = function()
+      vim.highlight.on_yank()
+   end,
 })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+   vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+   })
 end
 vim.opt.rtp:prepend(lazypath)
 
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,10 +1,10 @@
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {},
-	config = function()
-		vim.cmd([[colorscheme tokyonight]])
-		vim.cmd.hi("Comment gui=none")
-	end,
+   "folke/tokyonight.nvim",
+   lazy = false,
+   priority = 1000,
+   opts = {},
+   config = function()
+      vim.cmd([[colorscheme tokyonight]])
+      vim.cmd.hi("Comment gui=none")
+   end,
 }
