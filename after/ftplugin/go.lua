5dcf8785611181dec19e9aa60d53d6fda9627ba0
Boris <boris@gridscale.io>
2024-08-28 01:38:43 +0200

    some updates

---
+++ after/ftplugin/go.lua
@@ -0,0 +1,8 @@
+-- vim.opt_local.shiftwidth  = 4
+-- vim.opt_local.tabstop     = 4
+-- vim.opt_local.softtabstop = 4
+-- vim.opt_local.expandtab   = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = false
